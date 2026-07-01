a79cc6e6898850d5373c5ee618df95e84502b7c9
discostu21-zrp <stefanmeyer21@googlemail.com>
2026-07-01 15:41:39 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,16 @@
 
+// Karte erzeugen
+const map = L.map('map').setView([53.870530, 10.517432], 15);
+
+// OpenStreetMap
+L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: '© OpenStreetMap'
+    }
+).addTo(map);
+
+// Marker Marktplatz
+L.marker([53.870530, 10.517432])
+    .addTo(map)
+    .bindPopup("<b>📍 Marktplatz Zarpen</b><br>Start- und Zielpunkt aller Touren.");
