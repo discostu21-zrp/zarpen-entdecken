e20e567466aef6206f836c3b7f1e75edc808bb08
discostu21-zrp <stefanmeyer21@googlemail.com>
2026-07-06 20:32:21 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -9,6 +9,7 @@ const isDetail = mode === "detail";
 const map = L.map('map', {
   zoomControl: isDetail,
   scrollWheelZoom: isDetail,
+
   dragging: true,
   doubleClickZoom: isDetail,
   touchZoom: true
@@ -184,35 +185,130 @@ function highlightType(type) {
 
 // Infobox oben links
 const tourPanel = L.control({ position: 'topleft' });
+const tourPanel = L.control({ position: 'topleft' });
+
 tourPanel.onAdd = function () {
+
   const div = L.DomUtil.create('div', 'tour-panel');
+
   div.innerHTML = `
-    <h1>Redder & Hundewiese</h1>
-    <div class="subtitle">Zarpen entdecken – Spaziergang</div>
-    <div class="facts">
-      <div class="fact"><b>2,92 km</b><br>Distanz</div>
-      <div class="fact"><b>ca. 36 Min.</b><br>Dauer</div>
-      <div class="fact"><b>leicht</b><br>Schwierigkeit</div>
-      <div class="fact"><b>10 m</b><br>Höhenmeter</div>
-    </div>
-    <p>Abwechslungsreiche Runde durch den Redder, über die Hundespielwiese und zurück ins Dorf.</p>
-    <hr style="margin:18px 0;">
 
-<h3 style="margin:18px 0 10px;">Legende</h3>
+<details class="tour-details" open>
 
-<div class="legend-item" data-type="start" onclick="highlightType('start')"><span class="legend-icon marker-start">📍</span>Start/Ziel</div>
-<div class="legend-item" data-type="sehenswuerdigkeit" onclick="highlightType('sehenswuerdigkeit')"><span class="legend-icon marker-sehenswuerdigkeit">⛪</span>Sehenswürdigkeit</div>
-<div class="legend-item" data-type="gastronomie" onclick="highlightType('gastronomie')"><span class="legend-icon marker-gastronomie">🍴</span>Gastronomie</div>
-<div class="legend-item" data-type="service" onclick="highlightType('service')"><span class="legend-icon marker-service">ℹ️</span>Information / Schaukasten</div>
-<div class="legend-item" data-type="oeffentlich" onclick="highlightType('oeffentlich')"><span class="legend-icon marker-oeffentlich">🏛️</span>Öffentliche Einrichtung</div>
-<div class="legend-item" data-type="rast" onclick="highlightType('rast')"><span class="legend-icon marker-rast">🪑</span>Sitzbank</div>
-<div class="legend-item" data-type="hund" onclick="highlightType('hund')"><span class="legend-icon marker-hund">🐕</span>Hunde-Service</div>
-<div class="legend-item" data-type="natur" onclick="highlightType('natur')"><span class="legend-icon marker-natur">🌿</span>Natur & Aussicht</div>
-<div class="legend-item" data-type="warnung" onclick="highlightType('warnung')"><span class="legend-icon marker-warnung">!</span>Hinweis</div>
-  `;
+<summary>🌿 Tourinformationen</summary>
+
+<h1>Redder & Hundewiese</h1>
+
+<div class="subtitle">
+Zarpen entdecken – Spaziergang
+</div>
+
+<div class="facts">
+
+<div class="fact">
+<b>2,92 km</b><br>
+Distanz
+</div>
+
+<div class="fact">
+<b>ca. 36 Min.</b><br>
+Dauer
+</div>
+
+<div class="fact">
+<b>leicht</b><br>
+Schwierigkeit
+</div>
+
+<div class="fact">
+<b>10 m</b><br>
+Höhenmeter
+</div>
+
+</div>
+
+<p>
+Abwechslungsreiche Runde durch den Redder,
+über die Hundespielwiese und zurück ins Dorf.
+</p>
+
+<hr style="margin:18px 0;">
+
+<h3>Legende</h3>
+
+<div class="legend-item" data-type="start" onclick="highlightType('start')">
+<span class="legend-icon marker-start">
+📍
+</span>
+Start/Ziel
+</div>
+
+<div class="legend-item" data-type="sehenswuerdigkeit" onclick="highlightType('sehenswuerdigkeit')">
+<span class="legend-icon marker-sehenswuerdigkeit">
+⛪
+</span>
+Sehenswürdigkeit
+</div>
+
+<div class="legend-item" data-type="gastronomie" onclick="highlightType('gastronomie')">
+<span class="legend-icon marker-gastronomie">
+🍴
+</span>
+Gastronomie
+</div>
+
+<div class="legend-item" data-type="service" onclick="highlightType('service')">
+<span class="legend-icon marker-service">
+ℹ️
+</span>
+Information / Schaukasten
+</div>
+
+<div class="legend-item" data-type="oeffentlich" onclick="highlightType('oeffentlich')">
+<span class="legend-icon marker-oeffentlich">
+🏛️
+</span>
+Öffentliche Einrichtung
+</div>
+
+<div class="legend-item" data-type="rast" onclick="highlightType('rast')">
+<span class="legend-icon marker-rast">
+🪑
+</span>
+Sitzbank
+</div>
+
+<div class="legend-item" data-type="hund" onclick="highlightType('hund')">
+<span class="legend-icon marker-hund">
+🐕
+</span>
+Hunde-Service
+</div>
+
+<div class="legend-item" data-type="natur" onclick="highlightType('natur')">
+<span class="legend-icon marker-natur">
+🌿
+</span>
+Natur & Aussicht
+</div>
+
+<div class="legend-item" data-type="warnung" onclick="highlightType('warnung')">
+<span class="legend-icon marker-warnung">
+!
+</span>
+Hinweis
+</div>
+
+</details>
+
+`;
+
   L.DomEvent.disableClickPropagation(div);
+
   return div;
+
 };
+
 if (isDetail) {
-  tourPanel.addTo(map);
+    tourPanel.addTo(map);
 }
